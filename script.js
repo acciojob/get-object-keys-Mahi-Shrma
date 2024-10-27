64d604c504b4d7eb7662b73f725f65fa8d62e1fd
Mahi-Shrma <151066048+Mahi-Shrma@users.noreply.github.com>
2024-10-27 16:33:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 //your JS code here. If required.
 let student={
-	name:"Mahi";
-}
+	name:"Mahi"
+};
 Object.prototype.getKeys = function(){
 	return Object.keys(this);
-}
+};
 
 console.log(student.getKeys());
